32a34b3b40fd91bf5f54081ac6dd94b05c7d38bd
Richie <48403322+Richie-Z@users.noreply.github.com>
2026-04-07 10:59:38 +0700

    feat: update

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,49 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
-return {
+local spec = {
   "AstroNvim/astrocommunity",
+  -- UI
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.color.tint-nvim" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+
+  -- LSP
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.actions-preview-nvim" },
+
+  -- Git
+  { import = "astrocommunity.git.blame-nvim" },
+
+  -- Completion
+
+  -- Icons
+  { import = "astrocommunity/icon/mini-icons" },
+
+  -- Plugins
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+
+  -- Motions
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.marks-nvim" },
+
+  -- Packs
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.biome" },
 }
+
+if not os.getenv "SSH_TTY" then
+  local local_packs = { "go", "markdown", "java", "bash", "rust", "docker", "terraform", "svelte" }
+  for _, pack in ipairs(local_packs) do
+    table.insert(spec, { import = "astrocommunity.pack." .. pack })
+  end
+end
+
+return spec
