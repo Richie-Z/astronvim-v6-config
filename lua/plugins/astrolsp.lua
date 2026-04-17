afed91adb198f8a00b0d6543b16a1e7abc283a10
Richie <48403322+Richie-Z@users.noreply.github.com>
2026-04-17 10:13:24 +0700

    feat: update

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,9 +1,6 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
--- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
@@ -12,15 +9,15 @@ return {
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
+      codelens = true,
+      inlay_hints = false,
+      semantic_tokens = true,
     },
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,10 +1,22 @@
+-- AstroUI provides the basis for configuring the AstroNvim User Interface
+
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "catppuccin-mocha",
+    -- change colorscheme
+    colorscheme = "catppuccin-nvim",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    highlights = {
+      init = { -- this table overrides highlights in all themes
+      },
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+      },
+    },
+    -- Icons can be configured throughout the interface
     icons = {
+      -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
       LSPLoading3 = "⠹",
